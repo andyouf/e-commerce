b043b6a8946f87316e4b6cd813933db8eec05c04
andyouf <andrewossi1@gmail.com>
2020-12-16 20:48:30 -0600

    seeder sample data added

---
+++ backend/data/categories.js
@@ -0,0 +1,12 @@
+const categories = [
+    {
+        name: 'Electronics',
+        description: 'World Famous Electronics Products'
+    },
+    {
+        name: 'Shoes',
+        description: 'World Famous Electronics Products'
+    },
+]
+
+export default categories
