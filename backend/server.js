d82c290910d21be84798c55a4d892dc0cfdaf8c7
andyouf <andrewossi1@gmail.com>
2021-01-20 06:36:46 -0600

    deployed into heroku

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,16 @@ import dotenv from 'dotenv'
 dotenv.config()
 import morgan from 'morgan'
 import passport from 'passport'
-import { notFound, errorHandler } from './backend/middleware/errorMiddleware.js'
-import connectDB from './backend/config/db.js'
-import productRoutes from './backend/routes/productRoutes.js'
-import categoryRoutes from './backend/routes/categoryRoutes.js'
-import subCategoryRoutes from './backend/routes/subCategoryRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+import categoryRoutes from './routes/categoryRoutes.js'
+import subCategoryRoutes from './routes/subCategoryRoutes.js'
 // import generationRoutes from './routes/generationRoutes.js'
-import userRoutes from './backend/routes/userRoutes.js'
-import orderRoutes from './backend/routes/orderRoutes.js'
-import uploadRoutes from './backend/routes/uploadRoutes.js'
-import cartRoutes from './backend/routes/cartRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
+import cartRoutes from './routes/cartRoutes.js'
 connectDB()
 
 const app = express()
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json())
-import './backend/config/passport.js'
+import './config/passport.js'
 
 app.use('/api/products', productRoutes)
 app.use('/api/categories', categoryRoutes)
